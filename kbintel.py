40344c9361dc828178dc55a55a11c0972ff79a8e
Flavius Popan <flaviuspopan@gmail.com>
2014-03-20 00:43:33 -0400

    Basic crawl capabilities, only counts kills for each day of current month

--- kbintel.py
+++ kbintel.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 from tqdm import *
 from common import Browser
 import pdb
+from collections import defaultdict
 
 # Seconds to wait before each request
 TIMEOUT = 2
@@ -66,25 +67,24 @@ def fetch(url):
     else:
         fetch_url = url
 
-    # Clears the screen for prettiness
-    os.system('clear')
-    print "Retrieving data - please wait..."
+    clear()
+    print "Crawler initializing - please wait..."
 
     # Opens the specified URL and reads the home page
-    home_page = soupify(fetch_url)
+    #home_page = soupify(fetch_url)
     
     # Parses the URL for the top pilot and 
-    pilot_url = home_page.find_all("table", class_='kb-table awardbox')[0].a['href']
-    pilot_page = soupify(pilot_url)
+    #pilot_url = home_page.find_all("table", class_='kb-table awardbox')[0].a['href']
+    #pilot_page = soupify(pilot_url)
 
     # Parse
-    corp_name = pilot_page.find_all('tr', class_='kb-table-row-even')[0].find_all('a')[0].string
-    alliance_name = pilot_page.find_all('tr', class_='kb-table-row-even')[1].find_all('a')[0].string
+    #corp_name = pilot_page.find_all('tr', class_='kb-table-row-even')[0].find_all('a')[0].string
+    #alliance_name = pilot_page.find_all('tr', class_='kb-table-row-even')[1].find_all('a')[0].string
 
-    print 'Analyzing {}[{}]'.format(corp_name, alliance_name)
+    #print 'Analyzing {}[{}]'.format(corp_name, alliance_name)
     
     # Finds the kill page from the main page and feeds it into BeautifulSoup for parsing
-    browser.click_link(text='Home')
+    browser.open(fetch_url)
     kill_page = soupify(browser.click_link(text='Kills'))
     
     # Counts the number of pages to parse
@@ -109,11 +109,60 @@ def soupify(url):
     return BeautifulSoup(browser.response().read())
 
 
-def collect(first_page, page_count):
-    print "Generating Report"
-    #pdb.set_trace()
-    for page in tqdm(range(page_count), desc='Pages mined:'):
-        time.sleep(2)
+def clear():
+    # Clears the screen for prettiness
+    os.system('clear')
+
+
+def collect(first_page, pages):
+
+    # Start the crawler on the first page and modify the page variable over iterations
+    current_page = first_page
+
+    # Grabs the month
+    kill_month = browser.title().split('- ')[1]
+    print "Generating Report for {}".format(kill_month)
+
+    # Initialize counter variables and data arrays
+    next_page_counter = 1
+    data_dict = defaultdict(list)
+
+    # Need to preserve date order, which a dictionary can't do
+    ordered_keys = []
+
+    for page in tqdm(range(pages), desc='Pages Mined'):
+        days = current_page.find_all('div', class_='kb-date-header')
+        tables = current_page.find_all('table', class_='kb-table kb-kl-table kb-table-rows')
+        #pdb.set_trace()
+        for day, table in zip(days, tables):
+            if not day.string in ordered_keys:
+                ordered_keys.extend([day.string])
+            rows = table.tbody.find_all('tr')
+            for row in rows:
+                kill_location = row.find_all('div', class_='no_stretch kl-location')[0].text.split('\t ')[1]
+                kill_time = row.find_all('div', class_='kl-date')[0].text.split('\n')[1]
+                kill_data = [kill_location, kill_time]
+                data_dict[day.string].append(kill_data)
+        # Only increment page number if it's not on the last page
+        if next_page_counter < pages:
+            next_page_counter += 1
+            current_page = soupify(browser.click_link(text='%s' % str(next_page_counter)))
+    analyze(data_dict, ordered_keys)
+
+
+def analyze(data, ordered_keys):
+    # Serious unpacking going on - it's like inception up in this b
+    for day in ordered_keys:
+
+        kills = data[day]
+        kill_locations = []
+        kill_times = []
+        for kill in kills:
+            kill_locations.extend([kill[0]])
+            kill_times.extend([kill[1]])
+
+        print str(day)+': '
+        print len(kills)
 
 if __name__ == '__main__':
     main()
