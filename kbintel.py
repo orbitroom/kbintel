7dd18afb26ef669b63868b9e391c8a7efe37625a
Flavius Popan <flaviuspopan@gmail.com>
2014-07-19 18:48:24 -0400

    Started porting to zKillboard API

--- kbintel.py
+++ kbintel.py
@@ -1,37 +1,75 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+"""Killboard Intel
+
+Usage:
+  kbintel.py URL [-v] [-d]
+  kbintel.py -h | --help
+  kbintel.py --version
+
+Options:
+  -v --verbose  Show more text.
+  -d --debug    Show debug messages.
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
 
 import sys
 import os
-import time
-import argparse
-from bs4 import BeautifulSoup
-from tqdm import *
-from common import Browser
+import sqlite3
+import ConfigParser
+from docopt import docopt
+import zkb
 import pdb
-import collections
-from collections import defaultdict
-
-# Seconds to wait before each request
-TIMEOUT = 2
-
-VERSION = "0.1"
-
-# Instantiates a new browser object with specified timeout variable
-browser = Browser(TIMEOUT)
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Killboard Intel - Version: %s" % VERSION)
-    parser.add_argument("-u", "--url", help="generate this month's report from url")
-    args = parser.parse_args()
+# Read config vars from config.ini
+conf = ConfigParser.ConfigParser()
+conf.read("config.ini")
+db_schema = conf.get("GLOBAL", "db_name")
 
-    if args.url:
-        fetch(args.url)
+# Set global vars
+VERSION = "0.3"
+cursor = None
+db = None
+verbose = False
+debug = False
+
+
+# Set global vars based on args
+def set_globals():
+    global verbose, debug
+    if arguments['--verbose']:
+        verbose = True
+    if arguments['--debug']:
+        print arguments
+
+
+# Initialize database connection
+def db_init():
+    # TODO: Add search functionality to dynamically find db
+    # TODO: Add option to automatically download latest sqlite3 db
+    # TODO: Add print statements to log
+    global cursor, db, verbose
+
+    # Validate if path in config points to a file
+    if os.path.isfile(db_schema):
+        try:
+            db = sqlite3.connect(db_schema)
+            cursor = db.cursor()
+            if verbose:
+                print "DB connection:\tGOOD"
+        except Exception, e:
+            print "Database Error:" + e
+            sys.exit(2)
     else:
-        parser.print_help()
+        # TODO: Implement optional db connection - simple mode
+        sys.exit("SQLite Database Not Found! Verify path in config.ini")
 
 
+# I know, total overkill
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via raw_input() and return their answer.
 
@@ -44,7 +82,7 @@ def query_yes_no(question, default="yes"):
     """
     valid = {"yes": True, "y": True, "ye": True,
              "no": False, "n": False}
-    if default == None:
+    if default is None:
         prompt = " [y/n] "
     elif default == "yes":
         prompt = " [Y/n] "
@@ -61,7 +99,7 @@ def query_yes_no(question, default="yes"):
         elif choice in valid:
             return valid[choice]
         else:
-            sys.stdout.write("Please respond with 'yes' or 'no' " \
+            sys.stdout.write("Please respond with 'yes' or 'no' "
                              "(or 'y' or 'n').\n")
 
 
@@ -75,111 +113,16 @@ def fetch(url):
     clear()
     print "Crawler initializing - please wait..."
 
-    # Opens the specified URL and reads the home page
-    #home_page = soupify(fetch_url)
-
-    # Parses the URL for the top pilot and 
-    #pilot_url = home_page.find_all("table", class_='kb-table awardbox')[0].a['href']
-    #pilot_page = soupify(pilot_url)
-
-    # Parse
-    #corp_name = pilot_page.find_all('tr', class_='kb-table-row-even')[0].find_all('a')[0].string
-    #alliance_name = pilot_page.find_all('tr', class_='kb-table-row-even')[1].find_all('a')[0].string
-
-    #print 'Analyzing {}[{}]'.format(corp_name, alliance_name)
-
-    # Finds the kill page from the main page and feeds it into BeautifulSoup for parsing
-    browser.open(fetch_url)
-    kill_page = soupify(browser.click_link(text='Kills'))
-
-    # Grab the last page count container and find the number of pages
-    page_count = int(kill_page.find_all("div", class_='klsplitter')[-1].find_all('a')[1].string)
-
-    # Prompt for continuation if collection will take longer than 60 seconds
-    if page_count >= 100:
-        if query_yes_no(str(page_count) + ' pages of data found. Proceed?'):
-            collect(kill_page, page_count)
-        else:
-            print "Exiting"
-    else:
-        collect(kill_page, page_count)
-
-
-# BeautifulSoup helper function - returns BS page
-def soupify(url):
-    browser.open(url)
-    return BeautifulSoup(browser.response().read())
-
 
 def clear():
     # Clears the screen for prettiness
     os.system('clear')
 
 
-def collect(first_page, pages):
-    # Start the crawler on the first page and modify the page variable over iterations
-    current_page = first_page
-
-    # Grabs the month
-    #pdb.set_trace()
-    kill_month = browser.title().split('- ')[1]
-
-    # Grabs the alliance/corp name
-    title = current_page.findAll(attrs={"name":"description"})[0]['content'].encode('utf-8').split('- ')[1]
-
-    print title + ' - ' + kill_month
-
-    # Initialize counter variables and data arrays
-    next_page_counter = 1
-    data_dict = defaultdict(list)
-
-    # Need to preserve date order, which a dictionary can't do
-    ordered_keys = []
-
-    for page in tqdm(range(pages), desc='Pages Mined'):
-        days = current_page.find_all('div', class_='kb-date-header')
-        tables = current_page.find_all('table', class_='kb-table kb-kl-table kb-table-rows')
-        for day, table in zip(days, tables):
-            if not day.string in ordered_keys:
-                ordered_keys.extend([day.string])
-            rows = table.tbody.find_all('tr')
-            for row in rows:
-                kill_location = row.find_all('div', class_='no_stretch kl-location')[0].text
-                # Some version use different formatting
-                if kill_location.find('\t ') != -1:
-                    kill_location = kill_location.split('\t ')[1]
-                else:
-                    kill_location = kill_location.split('\n')[1]
-                kill_time = row.find_all('div', class_='kl-date')[0].text.split('\n')[1]
-                kill_data = [kill_location, kill_time]
-                data_dict[day.string].append(kill_data)
-        # Only increment page number if it's not on the last page
-        if next_page_counter < pages:
-            next_page_counter += 1
-            current_page = soupify(browser.click_link(text='%s' % str(next_page_counter)))
-    analyze(data_dict, ordered_keys)
-
-
-def analyze(data, ordered_keys):
-    # Serious unpacking going on - it's like inception up in this b
-    for day in ordered_keys:
-
-        kills = data[day]
-        kill_locations = []
-        kill_times = []
-        for kill in kills:
-            kill_locations.extend([kill[0]])
-            kill_times.extend([kill[1]])
-
-        kill_counter = collections.Counter(kill_locations)
-        print '--------------------------------'
-        print str(day)
-        print 'Kills: {}'.format(len(kills))
-        for item in kill_counter.items():
-            print str(item[0]) + ' - ' + str(item[1])
-        print '--------------------------------'
-        print '\n'
-
-
 if __name__ == '__main__':
-    main()
+    arguments = docopt(__doc__, version=VERSION)
+    set_globals()
+    db_init()
+    # Validate URL
+    #pdb.set_trace()
+    #zkb.fetch(arguments['URL'])
